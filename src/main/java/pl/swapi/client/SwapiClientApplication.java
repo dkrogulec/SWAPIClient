b7860aab17951b1c2b405684195ed5f94f3526cf
Daniel Krogulec <daniel.krogulec@gmail.com>
2018-03-15 12:45:30 +0100

    Initial commit

---
+++ src/main/java/pl/swapi/client/SwapiClientApplication.java
@@ -0,0 +1,12 @@
+package pl.swapi.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwapiClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwapiClientApplication.class, args);
+	}
+}
---
+++ src/main/java/pl/swapi/client/controllers/SampleController.java
@@ -0,0 +1,19 @@
+package pl.swapi.client.controllers;
+
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Daniel Krogulec
+ */
+@RestController
+public class SampleController {
+
+    @RequestMapping(path = "/greeting", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity greeting() {
+        return ResponseEntity.ok("Hello!");
+    }
+}
