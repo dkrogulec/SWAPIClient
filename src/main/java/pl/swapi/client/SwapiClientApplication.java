d1e8acd8febf5f859c8b37dda30b10e8480e1933
Daniel Krogulec <daniel.krogulec@gmail.com>
2018-03-15 13:06:50 +0100

    Author added

--- src/main/java/pl/swapi/client/SwapiClientApplication.java
+++ src/main/java/pl/swapi/client/SwapiClientApplication.java
@@ -3,6 +3,9 @@ package pl.swapi.client;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author Daniel Krogulec
+ */
 @SpringBootApplication
 public class SwapiClientApplication {
 
